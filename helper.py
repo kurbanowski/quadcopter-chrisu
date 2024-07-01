6ecfc3ddc8c883995d7f3c9e24e99148f0ae0085
chrisu <christopher.urbanowski@gmail.com>
2024-07-02 00:42:31 +0200

    fix: changed traversed_transmitters to a set()

--- helper.py
+++ helper.py
@@ -1,12 +1,11 @@
-def find_connected_transmitters(root, transmitters, traversed_transmitters, max_r):
+def find_connected_transmitters(root, transmitters, traversed_transmitters: set, max_r):
     for transmitter in transmitters:
         if transmitter not in traversed_transmitters:
-            traversed_transmitters.append(transmitter)
+            traversed_transmitters.add(transmitter)
             adjusted_range = transmitter.r + max_r + 1
             transmitters_in_range = root.find_in_circle(root, transmitter.x, transmitter.y, adjusted_range)
             connected_transmitters = []
             for t in transmitters_in_range:
-                traversed_transmitters.append(transmitter)
                 if t.connected:
                     # skip already connected transmitters
                     continue
@@ -15,6 +14,7 @@ def find_connected_transmitters(root, transmitters, traversed_transmitters, max_
                     if in_range:
                         connected_transmitters.append(t)
                         t.connect()
+                        traversed_transmitters.add(transmitter)
 
             find_connected_transmitters(root, connected_transmitters, traversed_transmitters, max_r)
     return False
